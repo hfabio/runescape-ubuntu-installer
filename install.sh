75bf1a680ba3bb9034f3e2e031da2dbe5798c65e
Helton Fábio <heltonfabio@outlook.com>
2020-10-04 16:28:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ wget -O - https://content.runescape.com/downloads/ubuntu/runescape.gpg.key | apt
 mkdir -p /etc/apt/sources.list.d
 echo "deb https://content.runescape.com/downloads/ubuntu trusty non-free" > /etc/apt/sources.list.d/runescape.list
 apt-get update
-apt-get install -y runescape-launcher
+apt-get install -y runescape-launcher &&
 rm -f /etc/apt/sources.list.d/runescape.list
 
 sed -i -e "s+Exec=+Exec=gamemoderun +g" /usr/share/applications/runescape-launcher.desktop
